b4b6561d04e9af39e59e023f0337b8ba062529cd
lizkavalski <lizkavalski@DESKTOP-IM8BUGM.localdomain>
2019-06-25 19:41:16 -0700

    framework

---
+++ starter-code/__tests__/arithmetic.test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const arithmetic = require('../lib/arithmetic.js');
+
+it()
---
+++ starter-code/lib/arithmetic.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let arithmetic = module.exports = {};
+
+arithmetic.add = function(a,b) {
+  if( typeof a !== "number" || typeof b !== "number" ) { return null; }
+  return a+b;
+};
+
+arithmetic.subtract = function (a,b) {
+  if( typeof a !== "number" || typeof b !== "number" ) { return null; }
+  return a-b;
+};
+
+arithmetic.mulitbly = function(a,b) {
+  if ( typeof a !== "number" || typeof b !== "number"){return null;}
+  return a*b;
+};
+
+arithmetic.divide = function(a,b) {
+  if (typeof a !== "number" || typeof b !== "number"){return null;}
+  if ( b <= 0){return "Please pick a number greater then zero";}
+  return a/b
+};
+
